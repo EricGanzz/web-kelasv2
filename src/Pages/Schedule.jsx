4254dedb279d883f6a9140d857db58ce5ddb2bd9
EricDev <90078706+EricGanzz@users.noreply.github.com>
2024-08-15 17:15:03 +0700

    Add files via upload

--- src/Pages/Schedule.jsx
+++ src/Pages/Schedule.jsx
@@ -21,10 +21,10 @@ const Schedule = () => {
 
     // Menentukan kelompok piket berdasarkan hari
     piketGroup = [
-        ["Amel", "Helmalia", "Fadli I", "Shera", "Nicollas"],
+        ["Eric", "Budi", "Fadli I", "Shera", "Nicollas"],
         ["Windi", "Lista", "Jeriko", "Pratiwi", "Damar"],
         ["Putri", "Paulista", "Firdaus", "Attala", "Ardian"],
-        ["Cariska", "Nila", "Wahid", "Togi", "Annisa"],
+        ["Eric", "Nila", "Wahid", "Togi", "Annisa"],
         ["Sultan", "Zaini", "Fajri", "Arif"],
     ]
 
