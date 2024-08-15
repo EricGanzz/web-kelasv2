a1aae62f238cfbb30b5c435910d7f9978825a718
EricDev <90078706+EricGanzz@users.noreply.github.com>
2024-08-15 17:23:10 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,23 +8,23 @@ const Home = () => {
 		<div className="text-white px-[10%]" id="Home">
 			<Navbar />
 			<div className="lg:hidden">
-				<div className="font-bold text-5xl text-center py-20 ">IX-5</div>
+				<div className="font-bold text-5xl text-center py-20 ">IX 5 CLASS </div>
 				<div
 					className="border-2 flex justify-between px-10 text-4xl font-bold py-2 relative"
 					id="TotalSiswa">
 					<div id="AngkaGradientBlue" className="flex items-center justify-center ">
-						25
+						35
 					</div>
 					<div className="">
 						<span id="AngkaGradientBlue" className="text-5xl">
-							3
+							2
 						</span>
 						<span id="AngkaGradientPink" className="text-5xl">
-							7
+							0
 						</span>
 					</div>
 					<div id="AngkaGradientPink" className="flex items-center justify-center">
-						12
+						15
 					</div>
 				</div>
 
